86987304607bc37921268fd40be43fe67ee90249
Mohamed Zaki <mamadovzicx@gmail.com>
2022-11-08 01:17:07 +0200

    Added main file to generate executable

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file main.cpp
+ * @author M Zaki (you@domain.com)
+ * @brief Entry point to create executable file
+ * @version 0.1
+ * @date 2022-11-07
+ * 
+ * @copyright Copyright (c) Zaki 2022
+ * 
+ */
+
+#include <iostream>
+
+#include <structs/structures.h>
+#include <gtest/gtest.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
