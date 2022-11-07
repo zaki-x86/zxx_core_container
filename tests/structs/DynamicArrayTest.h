d16060eadb95c57861032275612725f4ce7b3acd
Mohamed Zaki <mamadovzicx@gmail.com>
2022-11-08 01:14:35 +0200

    Added header files for test suites

---
+++ tests/structs/DynamicArrayTest.h
@@ -0,0 +1,49 @@
+/**
+ * @file DynamicArrayTest.h
+ * @author M Zaki (you@domain.com)
+ * @brief Test suite for DynamicArray class
+ * @version 0.1
+ * @date 2022-11-07
+ * 
+ * @copyright M Zaki Copyright (c) 2022
+ * 
+ */
+
+#include "structs/structures.h"
+#include "gtest/gtest.h"
+
+namespace structs
+{
+    namespace tests
+    {
+        /**
+         * @brief Test class for DynamicArray class.
+         * 
+         */
+        class DynamicArrayTest : public ::testing::Test
+        {
+        protected:
+            /**
+             * @brief Construct a new Dynamic Array Test object
+             * 
+             */
+            DynamicArrayTest();
+            /**
+             * @brief Destroy the Dynamic Array Test object
+             * 
+             */
+            ~DynamicArrayTest() override;
+            
+            /**
+             * @brief Class members declared here can be used by all tests in the test suite
+             * 
+             */
+            void method_placeholder();
+        };
+
+
+    } // namespace tests
+    
+    
+} // namespace structs
+
