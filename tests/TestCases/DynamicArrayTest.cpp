c926f2b11d53f0f8c3ebdc7423e6a9a83f69976d
Mohamed Zaki <mamadovzicx@gmail.com>
2022-11-09 16:48:58 +0200

    Updated test: InitializedWithArrayTest

--- tests/TestCases/DynamicArrayTest.cpp
+++ tests/TestCases/DynamicArrayTest.cpp
@@ -31,11 +31,11 @@ TEST_F(DynamicArrayTest, InitializedWithArrayTest)
 {
     // initialized with an (int) array
     EXPECT_EQ(ba0.size(), 5);
-    EXPECT_EQ(b0.get(0), 0);
-    EXPECT_EQ(b0.get(1), 1);
-    EXPECT_EQ(b0.get(2), 2);
-    EXPECT_EQ(b0.get(3), 3);
-    EXPECT_EQ(b0.get(4), 4);
+    EXPECT_EQ(ba0.get(0), 0);
+    EXPECT_EQ(ba0.get(1), 1);
+    EXPECT_EQ(ba0.get(2), 2);
+    EXPECT_EQ(ba0.get(3), 3);
+    EXPECT_EQ(ba0.get(4), 4);
 
     // .. TODO! ..
     // initialized with an (std::string) array
