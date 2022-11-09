353e28d5cb6a3c786898d0afe3233bcb5270bf7b
Mohamed Zaki <mamadovzicx@gmail.com>
2022-11-09 13:55:01 +0200

    Updated DynamicArray test class

--- tests/TestClasses/DynamicArrayTest.h
+++ tests/TestClasses/DynamicArrayTest.h
@@ -9,6 +9,61 @@
  * 
  */
 
+#pragma once
+
 #include "structs/structures.h"
 #include "gtest/gtest.h"
 
+namespace structs
+{
+    namespace tests
+    {
+        /**
+         * @brief Test class for DynamicArray class.
+         * 
+         */
+        class DynamicArrayTest : public ::testing::Test
+        {
+        protected:
+            structs::DynamicArray<int> a0;
+            structs::DynamicArray<int> a1;
+            structs::DynamicArray<int> a2;
+
+            /**
+             * @brief Construct a new Dynamic Array Test object
+             * 
+             */
+            DynamicArrayTest();
+            /**
+             * @brief Destroy the Dynamic Array Test object
+             * 
+             */
+            ~DynamicArrayTest() override;
+            
+            /**
+             * @brief Class members declared here can be used by all tests in the test suite
+             * 
+             */
+            void method_placeholder();
+        };
+
+
+    } // namespace tests
+    
+    
+} // namespace structs
+
+typedef structs::tests::DynamicArrayTest DynamicArrayTest;
+
+DynamicArrayTest::DynamicArrayTest()
+{
+    a1.push_back(-1);
+    a2.push_back(1);
+    a2.push_back(2);
+
+}
+
+DynamicArrayTest::~DynamicArrayTest()
+{
+
+}
