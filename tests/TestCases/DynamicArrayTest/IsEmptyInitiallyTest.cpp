dda380a8fa5137506b4d7126a04418d76925849a
Mohamed Zaki <mamadovzicx@gmail.com>
2022-11-09 15:00:26 +0200

    Re-organized tests directory
    It turns out seperating test cases of a single class will cause issues, will organize all tests associated with a dynamic list in a single .cpp file

--- tests/TestCases/DynamicArrayTest/IsEmptyInitiallyTest.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-#include "structs/structures.h"
-#include "TestClasses/DynamicArrayTest.h"
-
-TEST_F(DynamicArrayTest, IsEmptyInitially)
-{
-    EXPECT_EQ(a0.size(), 0);
-}
