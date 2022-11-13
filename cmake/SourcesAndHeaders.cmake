bf0a79a5b7d0f600a24e6dac6fbdba143e45f76b
Mohamed Zaki <mamadovzicx@gmail.com>
2022-11-13 17:00:14 +0200

    Updated top-level CMakeLists.txt:
    - Changed project name to `ZxContainers`
    - Added project options.
    - Setup alternative names
    - Added options for package managers: conan, vcpkg
    - Updated library headers and sources
    - Update project cxx_standard to 17 and warnings
    - Added alias to library
    - Added installation settings
    - Added version number settings
    - Included a separate for test (see next commit)

---
+++ cmake/SourcesAndHeaders.cmake
@@ -0,0 +1,37 @@
+set(sources
+  ${PROJECT_SOURCE_DIR}/src/main.cpp
+)
+
+set(exe_sources
+  ${PROJECT_SOURCE_DIR}/src/main.cpp
+  ${sources}
+)
+
+set(headers
+  ${PROJECT_SOURCE_DIR}/include/zx_containers/containers.hpp
+)
+
+set(test_sources
+  ${PROJECT_SOURCE_DIR}/test/runner.cpp
+  ${PROJECT_SOURCE_DIR}/test/test_suites/dynamic_array_test.cpp
+  ${PROJECT_SOURCE_DIR}/test/test_suites/binary_tree_test.cpp
+  ${PROJECT_SOURCE_DIR}/test/test_suites/bst_test.cpp
+  ${PROJECT_SOURCE_DIR}/test/test_suites/graph_test.cpp
+  ${PROJECT_SOURCE_DIR}/test/test_suites/hash_table_test.cpp
+  ${PROJECT_SOURCE_DIR}/test/test_suites/linked_list_test.cpp
+  ${PROJECT_SOURCE_DIR}/test/test_suites/queue_test.cpp
+  ${PROJECT_SOURCE_DIR}/test/test_suites/stack_test.cpp
+  ${PROJECT_SOURCE_DIR}/test/test_suites/red_black_tree_test.cpp
+)
+
+set(test_fixtures
+  ${PROJECT_SOURCE_DIR}/test/fixtures/dynamic_array_test.hpp
+  ${PROJECT_SOURCE_DIR}/test/fixtures/binary_tree_test.hpp
+  ${PROJECT_SOURCE_DIR}/test/fixtures/bst_test.hpp
+  ${PROJECT_SOURCE_DIR}/test/fixtures/graph_test.hpp
+  ${PROJECT_SOURCE_DIR}/test/fixtures/hash_table_test.hpp
+  ${PROJECT_SOURCE_DIR}/test/fixtures/linked_list_test.hpp
+  ${PROJECT_SOURCE_DIR}/test/fixtures/queue_test.hpp
+  ${PROJECT_SOURCE_DIR}/test/fixtures/stack_test.hpp
+  ${PROJECT_SOURCE_DIR}/test/fixtures/red_black_tree_test.hpp
+)
