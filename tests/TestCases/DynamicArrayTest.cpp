ca29def77d40cde51ca25b6cead0fc5786722ce1
Mohamed Zaki <mamadovzicx@gmail.com>
2022-11-13 13:19:04 +0200

    Updated test cases for DynamicArray class:
    - Added push_back tests that triggers resize

--- tests/TestCases/DynamicArrayTest.cpp
+++ tests/TestCases/DynamicArrayTest.cpp
@@ -13,6 +13,9 @@ using namespace structs::test;
 TYPED_TEST_SUITE_P(DynamicArrayTest);
 
 // #######################################################
+//
+// Constructor Test Cases
+//
 TYPED_TEST_P(DynamicArrayTest, IsEmptyInitially) 
 {
     EXPECT_EQ(this->a0_.size(), 0);
@@ -31,25 +34,29 @@ TYPED_TEST_P(DynamicArrayTest, InitializeWithArrayTest)
 
     EXPECT_EQ(xx.size(), TEST_INITIAL_SIZE);
 
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
+    for (long i = 0; i < TEST_INITIAL_SIZE; i++)
         EXPECT_EQ(xx.get(i), this->array_[i]);    
 }
 // #######################################################
-TYPED_TEST_P(DynamicArrayTest, PushBackMethodTest) 
+// #######################################################
+//
+// push_back() Test Cases - Array is not resized
+//
+TYPED_TEST_P(DynamicArrayTest, PushToEmptyArrayTest) 
 {
-    for (size_t i = 0; i < this->sample_values_.size(); i++)
+    for (long i = 0; i < this->sample_values_.size(); i++)
         this->a0_.push_back(this->sample_values_[i]);
     
     EXPECT_EQ(this->a0_.size(), this->sample_values_.size());
 
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
+    for (long i = 0; i < TEST_INITIAL_SIZE; i++)
         EXPECT_EQ(this->a0_.get(i), this->sample_values_[i]);
 }
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, SetMethodTest)
 {
-    size_t random_index = generate_numeric(0, TEST_INITIAL_SIZE - 1);
-    size_t initial_size = this->a1_.size();
+    long random_index = generate_numeric(0, TEST_INITIAL_SIZE - 1);
+    long initial_size = this->a1_.size();
 
     this->a1_.set(random_index, this->value_);
 
@@ -65,7 +72,7 @@ TYPED_TEST_P(DynamicArrayTest, SetAtIndexLessThanZeroTest)
      * @brief This test will always fail!
      * Since get() method accepts unsigned long params, the negative will be converted to unsigned integer value. This test will be skipped
      */
-    size_t random_index = -5;
+    long random_index = -5;
 
     std::cout << "[----------] " << "set at index: " << random_index 
     << ", this value: " << this->value_ << "\n";
@@ -77,8 +84,8 @@ TYPED_TEST_P(DynamicArrayTest, SetAtIndexLessThanZeroTest)
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, AddAtIndexGreaterThanZeroTest)
 {
-    size_t random_index = generate_numeric(1, TEST_INITIAL_SIZE);
-    size_t initial_size = this->a1_.size();
+    long random_index = generate_numeric(1, TEST_INITIAL_SIZE);
+    long initial_size = this->a1_.size();
 
     this->a1_.add(random_index, this->value_);
 
@@ -90,7 +97,7 @@ TYPED_TEST_P(DynamicArrayTest, AddAtIndexGreaterThanZeroTest)
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, AddAtIndexZeroTest)
 {
-    size_t initial_size = this->a1_.size();
+    long initial_size = this->a1_.size();
     TypeParam first_element = this->a1_.get(0);
 
     this->a1_.add(0, this->value_);
@@ -105,7 +112,7 @@ TYPED_TEST_P(DynamicArrayTest, AddAtIndexZeroTest)
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, AddToEmptyArrayTest)
 {
-    size_t random_index = generate_numeric(1, TEST_INITIAL_SIZE);
+    long random_index = generate_numeric(1, TEST_INITIAL_SIZE);
 
     EXPECT_ANY_THROW(this->a0_.add(random_index, this->value_));
 }
@@ -140,7 +147,7 @@ TYPED_TEST_P(DynamicArrayTest, PopArrayWithSingleElementTest)
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, PopRandomArrayTest)
 {
-    size_t initial_size = this->a1_.size();
+    long initial_size = this->a1_.size();
     TypeParam to_be_popped = this->a1_.get( initial_size - 1);
 
     EXPECT_EQ(to_be_popped, this->a1_.pop());
@@ -149,7 +156,7 @@ TYPED_TEST_P(DynamicArrayTest, PopRandomArrayTest)
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, RemoveFirstElementTest)
 {
-    size_t initial_size = this->a1_.size();
+    long initial_size = this->a1_.size();
     TypeParam to_be_deleted = this->a1_.get(0);
     TypeParam next_to_be_deleted = this->a1_.get(1);
 
@@ -174,20 +181,20 @@ TYPED_TEST_P(DynamicArrayTest, RemoveFromEmptyArrayTest)
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, RemoveLastElementTest)
 {
-    size_t initial_size = this->a1_.size();
+    long initial_size = this->a1_.size();
     TypeParam to_be_deleted = this->a1_.get(initial_size - 1);
     TypeParam previous_to_be_deleted = this->a1_.get(initial_size - 2);
 
     EXPECT_EQ(to_be_deleted, this->a1_.remove(initial_size - 1));
-    size_t curr_size = this->a1_.size();
+    long curr_size = this->a1_.size();
     EXPECT_EQ(curr_size, initial_size - 1);
     EXPECT_EQ(previous_to_be_deleted, this->a1_.get(curr_size - 1));
 }
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, RemoveElementRandomIndexTest)
 {
-    size_t initial_size = this->a1_.size();
-    size_t rand_index = generate_numeric<int>(1, initial_size - 2);
+    long initial_size = this->a1_.size();
+    long rand_index = generate_numeric<int>(1, initial_size - 2);
     TypeParam to_be_deleted = this->a1_.get(rand_index);
     TypeParam next_to_be_deleted =  this->a1_.get(rand_index + 1);
     TypeParam previous_to_be_deleted =  this->a1_.get(rand_index - 1);
@@ -200,20 +207,50 @@ TYPED_TEST_P(DynamicArrayTest, RemoveElementRandomIndexTest)
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, RemoveIndexOutOfRangeTest)
 {
-    size_t last_index = this->a1_.size() - 1;
-    size_t error_index = generate_numeric(last_index + 1, last_index + 100);
+    long last_index = this->a1_.size() - 1;
+    long error_index = generate_numeric(last_index + 1, last_index + 100);
 
     EXPECT_ANY_THROW(this->a1_.remove(error_index));
 }
 // #######################################################
-TYPED_TEST_P(DynamicArrayTest, ResizeMethodTest)
+// #######################################################
+//
+// resize() Test Cases for operations that trigger resize()
+//
+TYPED_TEST_P(DynamicArrayTest, PushBackCausesResizeSimpleTest) 
 {
-    
+   // Given: a1_.size() = 8, will add a random value 3 times to exceed initial size
+   this->a1_.push_back(this->value_);
+   this->a1_.push_back(this->value_);
+   this->a1_.push_back(this->value_);
+   
+   EXPECT_EQ(this->a1_.size(), 11);
+   EXPECT_EQ(this->a1_.capacity(), 20);
+   EXPECT_EQ(this->a1_.get(10), this->value_);
 }
 // #######################################################
-TYPED_TEST_P(DynamicArrayTest, IsEmptyMethodTest)
+TYPED_TEST_P(DynamicArrayTest, PatchPushBackCausesResizeSimpleTest) 
 {
-    
+    long samples_size = this->sample_values_.size();
+    long initial_size = this->a1_.size();
+
+    for (long i = 0; i < samples_size; i++)
+        this->a1_.push_back(this->sample_values_[i]);
+
+    long curr_size = this->a1_.size();
+
+    EXPECT_EQ(curr_size, samples_size + initial_size);
+    for (long i = initial_size; i < curr_size; i++)
+        EXPECT_EQ(this->a1_.get(i), this->sample_values_[i - initial_size]);
+}
+// #######################################################
+
+// #######################################################
+// #######################################################
+TYPED_TEST_P(DynamicArrayTest, IsEmptyAfterClearingArrayTest)
+{
+    this->a1_.reset();
+    EXPECT_EQ(this->a1_.size(), 0);
 }
 // #######################################################
 TYPED_TEST_P(DynamicArrayTest, ResetMethodTest)
@@ -222,7 +259,7 @@ TYPED_TEST_P(DynamicArrayTest, ResetMethodTest)
     EXPECT_EQ(this->a0_.size(), 0);
 
     // fill out with values
-    for (size_t i = 0; i < this->sample_values_.size(); i++)
+    for (long i = 0; i < this->sample_values_.size(); i++)
         this->a0_.push_back(this->sample_values_[i]);
     
     // test size
@@ -237,7 +274,7 @@ REGISTER_TYPED_TEST_SUITE_P(DynamicArrayTest,
     IsEmptyInitially,
     InitializeWithSingleElement,
     InitializeWithArrayTest,
-    PushBackMethodTest,
+    PushToEmptyArrayTest,
     SetMethodTest,
     SetAtIndexLessThanZeroTest,
     AddAtIndexGreaterThanZeroTest,
@@ -254,9 +291,10 @@ REGISTER_TYPED_TEST_SUITE_P(DynamicArrayTest,
     RemoveElementRandomIndexTest,
     RemoveFromSingleElementArrayTest,
     RemoveIndexOutOfRangeTest,
-    ResizeMethodTest,
+    PushBackCausesResizeSimpleTest,
+    PatchPushBackCausesResizeSimpleTest,
     ResetMethodTest,
-    IsEmptyMethodTest
+    IsEmptyAfterClearingArrayTest
     );
 
 using MyTypes = ::testing::Types<char, int, char*, int*, std::string> ;
