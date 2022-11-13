4cc518f105495b5e415b761d48573b349de72ca9
Mohamed Zaki <mamadovzicx@gmail.com>
2022-11-14 00:33:43 +0200

    Refactored DynamicArrayTest:
    Removed template specialization of constructor

--- test/fixtures/dynamic_array_test.hpp
+++ test/fixtures/dynamic_array_test.hpp
@@ -16,6 +16,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <type_traits>
+#include <memory>
 
 #include "zx_containers/containers.hpp"
 #include "gtest/gtest.h"
@@ -33,8 +34,6 @@ namespace zx_containers
         protected:
             void SetUp() override;
             void TearDown() override;
-            static void generate_random_test_value(T&);
-            static void generate_random_test_values(std::vector<T>&, long);
         public:
             zx_containers::darray<T> a0_;   // empty
             zx_containers::darray<T> a1_;   // filled upon initialization
@@ -50,86 +49,18 @@ using namespace zx_containers::test;
 template <typename T>
 void DynamicArrayTest<T>::SetUp()
 {
-}
-
-template <>
-void DynamicArrayTest<int>::SetUp()
-{
-    const int range_from  = INT32_MIN + 10;
-    const int range_to    = INT32_MAX - 10;
-    value_ = generate_numeric(range_from, range_to);
+    set_random_test_value(value_);
     
     for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        array_[i] = generate_numeric(range_from, range_to);
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        sample_values_.push_back(generate_numeric(range_from, range_to));
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-            a1_.push_back(generate_numeric(INT32_MIN + 10, INT32_MAX - 10));
-}
-
-template <>
-void DynamicArrayTest<char>::SetUp()
-{
-    value_ = generate_char();
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        array_[i] = generate_char();
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        sample_values_.push_back(generate_char());
+        set_random_test_value(array_[i]);
     
+    set_random_test_values(sample_values_);
+
     for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-            a1_.push_back(generate_char());
+            a1_.push_back(generate_random_test_value<T>());
 }
 
-template <>
-void DynamicArrayTest<char*>::SetUp()
-{
-    value_ = strdup(generate_string(generate_numeric(0, 20)).c_str());
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        array_[i] = strdup(generate_string(generate_numeric<int>(0, 20)).c_str());
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        sample_values_.push_back(strdup(generate_string(generate_numeric<int>(0, 20)).c_str()));
-    
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-            a1_.push_back(strdup(generate_string(generate_numeric<int>(0, 20)).c_str()));
-}
-
-template <>
-void DynamicArrayTest<int*>::SetUp()
-{
-    value_ = new int(generate_numeric<int>(INT32_MIN, INT32_MAX - 10));
-    sample_values_.reserve(TEST_INITIAL_SIZE);
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        array_[i] = new int(generate_numeric<int>(INT32_MIN + 10, INT32_MAX - 10));
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        sample_values_.push_back(new int(generate_numeric<int>(INT32_MIN + 10, INT32_MAX - 10)));
-    
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-            a1_.push_back(new int(generate_numeric<int>(INT32_MIN + 10, INT32_MAX - 10)));
-}
-
-template<>
-void DynamicArrayTest<std::string>::SetUp()
-{
-    value_ = generate_string(generate_numeric<int>(0, 20));
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        array_[i] = generate_string(generate_numeric<int>(0, 20));
-
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-        sample_values_.push_back(generate_string(generate_numeric<int>(0, 20)));
-    
-    for (size_t i = 0; i < TEST_INITIAL_SIZE; i++)
-            a1_.push_back(generate_string(generate_numeric<int>(0, 20)));
-}
-
+// --- Destructors ---
 template <typename T>
 void DynamicArrayTest<T>::TearDown()
 {
